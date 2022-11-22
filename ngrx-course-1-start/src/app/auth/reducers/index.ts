fea0be683aa5750b989ea0b93334560793934e71
Daniel Chanci <daniel.chancir@perficient.com>
2022-11-22 09:01:00 -0500

    Actions and reducers

---
+++ ngrx-course-1-start/src/app/auth/reducers/index.ts
@@ -0,0 +1,35 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  MetaReducer,
+  on
+} from '@ngrx/store';
+import { environment } from '../../../environments/environment';
+import { AuthActions } from '../actions-types';
+import { User } from '../model/user.model';
+
+export const authFeatureKey = 'auth';
+
+export const initialAuthState: AuthState = {
+  user: undefined,
+}
+
+export interface AuthState {
+  user: User
+}
+
+
+
+export const metaReducers: MetaReducer<AuthState>[] = !environment.production ? [] : [];
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user
+    }
+  })
+)
